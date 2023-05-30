267373284707899a6db9a7a90654bac3e0b5d1dd
Merchite <merchitee@gmail.com>
2023-05-30 22:26:24 +0200

    Updated AddProduct component

--- src/AddProduct.js
+++ src/AddProduct.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { TextField, Button } from '@mui/material';
+import { TextField, Button, FormControl, InputLabel, Select, MenuItem, Checkbox } from '@mui/material';
 
 const AddProduct = () => {
   const navigate = useNavigate();
   const [name, setName] = useState('');
   const [price, setPrice] = useState('');
   const [description, setDescription] = useState('');
+  const [image, setImage] = useState('https://via.placeholder.com/300x300');
+  const [status, setStatus] = useState('Draft');
+  const [channel, setChannel] = useState('Channel A');
+  const [needsRefinement, setNeedsRefinement] = useState(false);
+
 
   const handleNameChange = (e) => {
     setName(e.target.value);
@@ -20,6 +25,22 @@ const AddProduct = () => {
     setDescription(e.target.value);
   };
 
+  const handleImageChange = (e) => {
+    setImage(e.target.value);
+  };
+
+  const handleStatusChange = (e) => {
+    setStatus(e.target.value);
+  };
+
+  const handleChannelChange = (e) => {
+    setChannel(e.target.value);
+  };
+
+  const handleRefinementChange = (e) => {
+    setNeedsRefinement(e.target.checked);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -29,18 +50,45 @@ const AddProduct = () => {
       return;
     }
 
-    // Create new product object
-    const newProduct = {
-      name,
-      price: parseFloat(price),
-      description,
+    // Perform API call or data update logic here
+    const addProduct = async () => {
+      try {
+        const getResponse = await fetch('http://localhost:3030/products');
+        const getData = await getResponse.json();
+        
+        //Find the highest id from the existing products
+        const lastProduct = getData.reduce((maxId, product) => Math.max(maxId, product.id), 0);
+        const newProductId = lastProduct + 1;
+        // Create new product object
+        const newProduct = {
+          id: newProductId,
+          name,
+          price: parseFloat(price),
+          description,
+          image,
+          status,
+          channel,
+          needsRefinement,
+        };
+
+        const addResponse = await fetch('http://localhost:3030/products', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(newProduct),
+        });
+        const addData = await addResponse.json();
+        console.log('Product added:', data);
+      } catch (error) {
+        console.log('Error adding product:', error);
+      }
     };
 
-    // Perform API call or data update logic here
-    // ...
+    addProduct(newProduct);
 
     // Redirect to the product listing page
-    navigate('/products');
+    navigate(`/products/${newProductId}`);
   };
 
   return (
@@ -65,7 +113,44 @@ const AddProduct = () => {
           multiline
           rows={4}
         />
-        <Button variant="contained" color="primary" type="submit">Save</Button>
+        <TextField
+          label="Image"
+          value={image}
+          onChange={handleImageChange}
+        />
+        <FormControl>
+          <InputLabel id="selectStatus-label">Status</InputLabel>
+          <Select value={status} onChange={handleStatusChange} id="selectStatus" labelId="selectStatus-label">
+            <MenuItem value="Draft">Draft</MenuItem>
+            <MenuItem value="Active">Active</MenuItem>
+            <MenuItem value="Archived">Archived</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl>
+          <InputLabel id="selectChannel-label">Channel</InputLabel>
+          <Select value={channel} onChange={handleStatusChange} id="selectChannel" labelId="selectChannel-label">
+            <MenuItem value="Channel A">Channel A</MenuItem>
+            <MenuItem value="Channel B">Channel B</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl>
+          <Checkbox
+            label="Needs Refinement"
+            checked={needsRefinement}
+            onChange={handleRefinementChange}
+            inputProps={{ 'aria-label': 'controlled' }}
+          />
+        </FormControl>
+
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+        >
+          Save
+        </Button>
       </form>
     </div>
   );
